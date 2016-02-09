092cb64a73b1a24c2b5ca7170f16439d342e069b
Saamari <sampsa_kautto@hotmail.com>
2016-02-09 10:53:23 +0200

    moro

---
+++ Car count/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Car_count
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+           
+            
+            int num = int.Parse(textBlockTruck.Text);
+            num++;
+            textBlockTruck.Text = num.ToString();
+            
+            
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+           
+            int num1 = int.Parse(textBlockCar.Text);
+            num1++;
+            textBlockCar.Text= num1.ToString();
+
+        }
+    }
+}
